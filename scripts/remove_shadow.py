eb9150e35ebd9a1afa0e77cd3aae539306154966
Will Grayeski <113263803+Squeegy@users.noreply.github.com>
2023-12-20 14:35:01 -0500

    add a low and high threshold

--- scripts/remove_shadow.py
+++ scripts/remove_shadow.py
@@ -2,7 +2,7 @@ from PIL import Image
 import numpy as np
 import cv2
 
-def remove_shadow(pil_img, shadow_threshold=50, edge_threshold=100):
+def remove_shadow(pil_img, shadow_threshold=50, low_edge_threshold=50,  high_edge_threshold=100):
     # Convert Pillow image to OpenCV format in BGR
     open_cv_image_bgr = np.array(pil_img.convert('RGB'))[:, :, ::-1]
 
@@ -10,7 +10,7 @@ def remove_shadow(pil_img, shadow_threshold=50, edge_threshold=100):
     gray = cv2.cvtColor(open_cv_image_bgr, cv2.COLOR_BGR2GRAY)
 
     # Detect edges (presumably including black or almost black outlines)
-    edges = cv2.Canny(gray, edge_threshold, edge_threshold * 2)
+    edges = cv2.Canny(gray, low_edge_threshold, high_edge_threshold)
 
     # Dilate the edges a bit to ensure they are not removed with the shadow
     kernel = np.ones((3,3), np.uint8)
