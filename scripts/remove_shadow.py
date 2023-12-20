9f21304fa1ddc5770fdd2b374f86d3d1867926ad
Will Grayeski <113263803+Squeegy@users.noreply.github.com>
2023-12-20 14:19:42 -0500

    add edge detection that should help us avoid removing outlines

--- scripts/remove_shadow.py
+++ scripts/remove_shadow.py
@@ -2,29 +2,30 @@ from PIL import Image
 import numpy as np
 import cv2
 
-# Image needs to have shadows that are distinct enough from the outline of the image
+def remove_shadow(pil_img, shadow_threshold=50, edge_threshold=100):
+    # Convert Pillow image to OpenCV format in BGR
+    open_cv_image_bgr = np.array(pil_img.convert('RGB'))[:, :, ::-1]
 
-def remove_shadow(pil_img, shadow_threshold = 80):
-    # Load the image with Pillow
-
-    # Convert the Pillow image to an OpenCV image
-    open_cv_image = np.array(pil_img)
-    open_cv_image_bgr = open_cv_image[:, :, ::-1].copy()  # Convert RGB to BGR
-
-    # Convert image to grayscale for shadow detection
+    # Convert image to grayscale
     gray = cv2.cvtColor(open_cv_image_bgr, cv2.COLOR_BGR2GRAY)
 
-    # Apply thresholding to identify shadow
+    # Detect edges (presumably including black or almost black outlines)
+    edges = cv2.Canny(gray, edge_threshold, edge_threshold * 2)
+
+    # Dilate the edges a bit to ensure they are not removed with the shadow
+    kernel = np.ones((3,3), np.uint8)
+    edges_dilated = cv2.dilate(edges, kernel, iterations=1)
+
+    # Apply thresholding to identify shadow, excluding edges
     _, thresholded = cv2.threshold(gray, shadow_threshold, 255, cv2.THRESH_BINARY)
+    shadow_mask = cv2.bitwise_and(thresholded, thresholded, mask=~edges_dilated)
 
-    # Use the thresholded image to create an alpha channel
-    # Where the shadow is, set alpha to 0 (transparent)
-    b, g, r, a = cv2.split(open_cv_image)
-    a[thresholded == 0] = 0  # Set alpha to 0 where the shadow is detected
+    # Create an alpha channel for transparency
+    b, g, r, a = cv2.split(open_cv_image_bgr)
+    a[shadow_mask == 0] = 0  # Transparent where the shadow is detected
 
-    # Merge back the channels including the new alpha
-    open_cv_image = cv2.merge((b, g, r, a))
+    # Merge channels including the new alpha
+    open_cv_image_with_alpha = cv2.merge((b, g, r, a))
 
-    # Convert back to Pillow image
-    open_cv_image = cv2.cvtColor(open_cv_image, cv2.COLOR_BGRA2RGBA)
-    return Image.fromarray(open_cv_image)
+    # Convert back to Pillow image in RGBA format
+    return Image.fromarray(cv2.cvtColor(open_cv_image_with_alpha, cv2.COLOR_BGRA2RGBA))
