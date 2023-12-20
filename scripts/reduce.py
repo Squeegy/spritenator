f77f7c80883769b0518cdf21b699409b3480b6a8
Will Grayeski <113263803+Squeegy@users.noreply.github.com>
2023-12-20 10:03:48 -0500

    go by squares now. We want aliasing

--- scripts/reduce.py
+++ scripts/reduce.py
@@ -1,54 +1,35 @@
 from PIL import Image
 
-def reduce_image_size(img, fuzziness=0.9):
+def reduce_image_size(img):
     original_avg_color = average_color(img)
     width, height = img.size
 
-    # Start with a rectangle size that is a quarter of the image size
-    rect_width, rect_height = width // 4, height // 4
+    # Start with a square size that is a quarter of the smaller dimension of the image
+    square_size = min(width, height) // 4
 
-    while rect_width > 1 and rect_height > 1:
-        reduced_img = create_reduced_image(img, rect_width, rect_height)
+    while square_size > 1:
+        reduced_img = create_reduced_image(img, square_size)
         reduced_avg_color = average_color(reduced_img)
 
         if not is_color_difference_significant(original_avg_color, reduced_avg_color):
             break  # Acceptable color difference, stop shrinking
 
-        # Find the next largest divisors for width and height with fuzziness
-        rect_width = find_largest_divisor(width, rect_width, fuzziness)
-        rect_height = find_largest_divisor(height, rect_height, fuzziness)
+        square_size -= 1
 
     return reduced_img
 
-def find_largest_divisor(number, max_divisor, fuzziness):
-    # Fuzziness tolerance (e.g., 5% of the number)
-    tolerance = number * (1 - fuzziness)
-
-    closest_divisor = 1
-    smallest_remainder = number
-
-    for divisor in range(max_divisor - 1, 1, -1):
-        remainder = number % divisor
-
-        # Check if the remainder is within the tolerance range
-        if remainder <= tolerance:
-            if remainder < smallest_remainder:
-                closest_divisor = divisor
-                smallest_remainder = remainder
-
-    return closest_divisor
-
-def create_reduced_image(img, rect_width, rect_height):
-    new_width = img.width // rect_width
-    new_height = img.height // rect_height
+def create_reduced_image(img, square_size):
+    new_width = img.width // square_size
+    new_height = img.height // square_size
     new_img = Image.new('RGBA', (new_width, new_height))
 
     for i in range(new_width):
         for j in range(new_height):
-            central_pixel = img.getpixel((i * rect_width + rect_width // 2, j * rect_height + rect_height // 2))
+            central_pixel = img.getpixel((i * square_size + square_size // 2, j * square_size + square_size // 2))
             new_img.putpixel((i, j), central_pixel)
 
     return new_img
+
 
 def average_color(img):
     r, g, b = 0, 0, 0
