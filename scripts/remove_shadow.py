7f6d522bef76ac2ee88bd10e4359d36346420415
Will Grayeski <113263803+Squeegy@users.noreply.github.com>
2023-12-20 14:14:30 -0500

    correct how we handle alpha

--- scripts/remove_shadow.py
+++ scripts/remove_shadow.py
@@ -7,18 +7,17 @@ def remove_shadow(pil_img, shadow_threshold = 50):
 
     # Convert the Pillow image to an OpenCV image
     open_cv_image = np.array(pil_img)
-    open_cv_image = open_cv_image[:, :, ::-1].copy()  # Convert RGB to BGR
+    open_cv_image_bgr = open_cv_image[:, :, ::-1].copy()  # Convert RGB to BGR
 
     # Convert image to grayscale for shadow detection
-    gray = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(open_cv_image_bgr, cv2.COLOR_BGR2GRAY)
 
     # Apply thresholding to identify shadow
     _, thresholded = cv2.threshold(gray, shadow_threshold, 255, cv2.THRESH_BINARY)
 
     # Use the thresholded image to create an alpha channel
     # Where the shadow is, set alpha to 0 (transparent)
-    b, g, r, _ = cv2.split(open_cv_image)
-    a = np.ones(b.shape, dtype=b.dtype) * 255  # Create a fully opaque alpha channel
+    b, g, r, a = cv2.split(open_cv_image)
     a[thresholded == 0] = 0  # Set alpha to 0 where the shadow is detected
 
     # Merge back the channels including the new alpha
