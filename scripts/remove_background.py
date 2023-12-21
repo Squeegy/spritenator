f17af217404b3d480236b7c9499af2ab44b5ff77
Will Grayeski <113263803+Squeegy@users.noreply.github.com>
2023-12-21 07:22:06 -0500

    thresholding is pretty good. now let's blur to get rid of shadow edge

--- scripts/remove_background.py
+++ scripts/remove_background.py
@@ -111,7 +111,8 @@ def remove_background_and_clean_artifacts(image, tolerance=0.01, min_size=64):
     return new_image, color_block
 
 def brighten_light_areas(gray_img):
-    adaptive_thresh = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 13, 15)
+    blurred = cv2.GaussianBlur(gray_img, (5, 5), 0)
+    adaptive_thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 13, 15)
  
     return adaptive_thresh
 
